26e54cefc0f1528ca70feec18b5496f130e31e22
Stephen Homer Catacte <dev.scatacte@gmail.com>
2023-09-19 14:16:50 +0800

    signin api

---
+++ app/api/signin/route.tsx
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export const POST = async (req: Request) => {
+  try {
+    const data = await req.json();
+    const apiUrl = process.env.API_URL + 'auth?api_key=' + process.env.API_KEY;
+
+    const params = new URLSearchParams();
+    params.append('email', data.email);
+    params.append('password', data.password);
+
+    const res = await axios.post(apiUrl, params);
+
+    console.log('API_URL', apiUrl);
+
+    return NextResponse.json(res.data);
+  } catch (error) {
+    console.log('Error:', error);
+    // return error;
+  }
+};
