3750a7eb618f5b7f804eff405e136a2c7e69da41
andrebc52 <andreboncipriano@gmail.com>
2023-12-07 09:04:19 +0800

    andre update roadmap

--- components/Forms/roadmap.tsx
+++ components/Forms/roadmap.tsx
@@ -6,7 +6,7 @@ const VNOC_roadmap: React.FC = () => {
   
   return (
     <section id="features" className="py-20 lg:py-25 xl:py-30">
-      <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0" style={{height:2200}}>
+      <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0" style={{height:1800}}>
         <div className="lizard-relative">
           <Script src="https://manage.vnoc.com/taskmanager/public/dntrademark.com/36929?container=tasks" />
           <div id="tasks" className="tasks"></div>
